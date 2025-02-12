2b201e8c8b3632e9a5c4404781e55bc1421f2d4c
Kyusaan41 <kyusan_lol@outlook.fr>
2025-02-12 22:06:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎮 Playing By Kyû"];
+const statusMessages = ["🎮 By Kyû"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
